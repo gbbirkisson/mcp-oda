de5e326870aa679c3217b7789833a5bd0844ce68
Guðmundur Björn Birkisson <gbb@gbb.is>
2026-02-28 22:46:13 +0100

    chore(main): release 0.4.5

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-exec npx -y ${MCP_ODA_REF:-github:gbbirkisson/mcp-oda#0.4.4} mcp # x-release-please-version
+exec npx -y ${MCP_ODA_REF:-github:gbbirkisson/mcp-oda#0.4.5} mcp # x-release-please-version
